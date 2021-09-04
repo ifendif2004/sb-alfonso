19446ada0c82216f13b62554e022790f16195b32
ifendif2004 <ifendif2004@gmail.com>
2021-09-04 22:19:16 +0200

    Primer commit

--- src/main/java/com/alfonso/springbatch/listener/JobListener.java
+++ src/main/java/com/alfonso/springbatch/listener/JobListener.java
@@ -6,7 +6,7 @@ import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.listener.JobExecutionListenerSupport;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.OverrideAutoConfiguration;
+//import org.springframework.boot.test.autoconfigure.OverrideAutoConfiguration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
